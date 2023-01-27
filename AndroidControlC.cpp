1209d42e5bfea65cf138b7e51d022a3f2242897c
AndroidMusicVideos <122070241+AndroidMusicVideos@users.noreply.github.com>
2023-01-27 13:27:30 -0600

    Uploaded main .cpp file.

---
+++ AndroidControlC.cpp
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo baseServo;
+Servo shoulderServo;
+Servo elbowServo;
+Servo gripperServo;
+
+int basePos = 90;
+int shoulderPos = 90;
+int elbowPos = 90;
+int gripperPos = 90;
+
+void setup() {
+  baseServo.attach(9);
+  shoulderServo.attach(10);
+  elbowServo.attach(11);
+  gripperServo.attach(12);
+}
+
+void loop() {
+  baseServo.write(basePos);
+  shoulderServo.write(shoulderPos);
+  elbowServo.write(elbowPos);
+  gripperServo.write(gripperPos);
+}
